8ac1347402156713c1ee4a2f137ad0c38a6b1b19
Kohei Arai <a_la_mode1108@icloud.com>
2018-07-19 09:28:39 +0900

    Initial Commit

---
+++ LikeManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LikeManager
+//
+//  Created by Kohei Arai on 2018/07/19.
+//  Copyright © 2018年 Kohei Arai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
